8ab67aedb47d13e7ab093a42bc69eba168ad7732
crachmanin <crachmanin@gmail.com>
2017-11-29 16:39:38 -0700

    adding font detection

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
+import os
 from flask import Flask
 from flask import request, render_template
 
 app = Flask(__name__)
 
 
+def load_all_fonts():
+    with open(os.path.join("static", "fontlist.txt")) as fp:
+        fonts = [line.strip() for line in fp]
+        return fonts
+
+
 @app.route('/')
 def hello_world():
     headers = request.headers
-    return render_template("home_page.html", headers=dict(headers))
+    fonts = load_all_fonts()
+    return render_template("home_page.html", headers=dict(headers), fonts=fonts)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
