5f6cd72f3ac8b28645bc2335e73c3250416c4086
crachmanin <crachmanin@gmail.com>
2017-12-06 01:11:28 -0700

    live updates working on app engine

--- static/plot.js
+++ static/plot.js
@@ -1,7 +1,6 @@
 var MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var LABELS = [ "Fingerprint", "User Agent", "Accept", "Accept Encoding", "Accept Language", "Plugins", "Screen resolution", "Platform", "Hardware Concurrency", "Html Canvas Data", "Webgl Vendor", "Webgl Renderer", "Audio Sample Rate", "Audio Base Latency", "Fonts Available", "logged in to"]
 var DATA = JSON.parse(getData()).map(val => parseInt(val));
-console.log(DATA);
 
 function getData() {
   var result = null;
@@ -65,37 +64,3 @@ socket.on('response', function(msg) {
   barChartData.datasets[0].data = JSON.parse(msg.data);
   window.myBar.update();
 });
-
-//var colorNames = Object.keys(window.chartColors);
-//document.getElementById('addDataset').addEventListener('click', function() {
-  //var colorName = colorNames[barChartData.datasets.length % colorNames.length];;
-  //var dsColor = window.chartColors[colorName];
-  //var newDataset = {
-    //label: 'Dataset ' + barChartData.datasets.length,
-    //backgroundColor: color(dsColor).alpha(0.5).rgbString(),
-    //borderColor: dsColor,
-    //borderWidth: 1,
-    //data: []
-  //};
-
-  //for (var index = 0; index < barChartData.labels.length; ++index) {
-    //newDataset.data.push(randomScalingFactor());
-  //}
-
-  //barChartData.datasets.push(newDataset);
-  //window.myBar.update();
-//});
-
-//document.getElementById('addData').addEventListener('click', function() {
-  //if (barChartData.datasets.length > 0) {
-    //var month = MONTHS[barChartData.labels.length % MONTHS.length];
-    //barChartData.labels.push(month);
-
-    //for (var index = 0; index < barChartData.datasets.length; ++index) {
-      ////window.myBar.addData(randomScalingFactor(), index);
-      //barChartData.datasets[index].data.push(randomScalingFactor());
-    //}
-
-    //window.myBar.update();
-  //}
-//});
